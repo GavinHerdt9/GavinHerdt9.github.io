344d7e1a4c7c0f7bfd3bbe7e41078b3827b40625
Gavin Herdt <gherdt@outlook.com>
2022-10-18 17:12:19 +0000

    sped circles up

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var circles = [];
         // TODO 2 : Create a function that draws a circle 
 function drawCircle(){
     circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-    physikz.addRandomVelocity(circle, canvas);
+    physikz.addRandomVelocity(circle, canvas, 2, 5);
     view.addChild(circle);
     circles.push(circle);
 }
